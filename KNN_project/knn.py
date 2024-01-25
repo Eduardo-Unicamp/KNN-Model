0f8c3bd921bb2d5e177d7d218ea68747d8027ef7
Edu <eduardojuniorsilvasaldanhacmd@gmail.com>
2024-01-25 08:52:02 -0300

    knn function now returns a list with distances

--- KNN_project/knn.py
+++ KNN_project/knn.py
@@ -1,4 +1,4 @@
-points = {'categoryA' : [[1,2],[1,4],[3,2],[4,2],[3,4],], 'categoryB': [[10,20],[11,22],[14,10],[21,22],[21,19]]}#pontos iniciais
+
 
 
 def euclidean_distance(dimensions:int, initial_point_coordinates:list,final_point_coordinates:list):
@@ -17,7 +17,21 @@ def euclidean_distance(dimensions:int, initial_point_coordinates:list,final_poin
 
 
 
-def knn(dimensions,*coordinates, initial_points = points):#recebe o numero de dimensões e as coordenadas, esse poinsts por padrão pode tirar depois
-    pass
+def knn(data:list,analysed_point_coordinates:tuple):
+    distances_list = []
+    
+    #calcular todas as distancias
+    for client in data:
+        values_tuple = client[2]
+        distances_list.append(euclidean_distance(len(values_tuple),list(values_tuple),list(analysed_point_coordinates)))
+        
+    print(distances_list)
 
+
+
+    #ver quais as k menores
+
+
+    
+    #dar a resposta com base nisso
     
