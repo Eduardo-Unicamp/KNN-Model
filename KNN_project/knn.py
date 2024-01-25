8fac1a03629e935eda905c6b2fcf9e67da8d0667
Edu <eduardojuniorsilvasaldanhacmd@gmail.com>
2024-01-25 11:53:27 -0300

    versão que já retorna a lista de k listas category,distance agora é fazer a contagem de quantas de cada categoria e entregar o resultado

--- KNN_project/knn.py
+++ KNN_project/knn.py
@@ -18,17 +18,39 @@ def euclidean_distance(dimensions:int, initial_point_coordinates:list,final_poin
 
 
 def knn(data:list,analysed_point_coordinates:tuple, k = 3):
-    distances_list = []
+    distances_dict = {}
     
     #calcular todas as distancias
     for client in data:
         values_tuple = client[2]
-        distances_list.append(euclidean_distance(len(values_tuple),list(values_tuple),list(analysed_point_coordinates)))
+        distances_dict.update({client[0]:euclidean_distance(len(values_tuple),list(values_tuple),list(analysed_point_coordinates))})
         
     #ver quais as k menores
-    sorted_list = sorted(distances_list)
+    sorted_list = sorted(distances_dict.items(),key=lambda x:x[1])
     k_nearest = sorted_list[0:k]
 
-    print(sorted_list, '\n\n\n', k_nearest) 
+    #descobrir a categoria de cada cpf de k_nearest : já que como o dict nao aceita chave repetida tem identificar a distancia 
+    #pelo cpf primeiro(o qual nao repete) e só depois ver qual o perfil daquele cpf
+    category = ''
+    k_nearest_new = []
+
+    for cpf_distance in k_nearest:
+        cpf = cpf_distance[0]
+        distance = cpf_distance[1]
+
+        for client in data:
+            if cpf in client:
+                category = client[1]
+                k_nearest_new.append([category,distance])
+    print(k_nearest_new)
+        
+
+
+
+
+    #print(distances_dict,'\n\n',sorted_list,'\n\n',k_nearest,'\n\n')
+
+
     #dar a resposta com base nisso
     
+    #---------------COMMENT TO AVOID GETTING LOOSSSSSSSTTT IN CTRL-Z -------------------------------------------------------------#
